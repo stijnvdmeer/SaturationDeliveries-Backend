aa4ffe61694a1c3b11ad28dde109f2b5788e87f2
stijnvdmeer <stijn08072002@outlook.com>
2025-04-12 12:39:19 +0200

    Created category Model

---
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/models/Category.java
@@ -0,0 +1,34 @@
+package nl.svdm.saturationdeliveriesbackend.models;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+    public Category() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
