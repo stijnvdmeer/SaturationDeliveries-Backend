f93aa38146149a1979b5b6cd1243358a86e1a28b
stijnvdmeer <stijn08072002@outlook.com>
2025-04-13 10:04:18 +0200

    finished product model

--- src/main/java/nl/svdm/saturationdeliveriesbackend/models/Product.java
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/models/Product.java
@@ -1,4 +1,49 @@
 package nl.svdm.saturationdeliveriesbackend.models;
 
+import jakarta.persistence.*;
+import nl.svdm.saturationdeliveriesbackend.models.embeds.ProductNutrition;
+import nl.svdm.saturationdeliveriesbackend.models.enums.ProductLabel;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToOne
+    private SubCategory subCategory;
+
+    @OneToMany(mappedBy = "product")
+    private List<OrderItem> orderItems;
+
+    private String name;
+    private Double price;
+    private Integer stock;
+    private Integer pending;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<ProductLabel> labels;
+
+    @Embedded
+    private ProductNutrition nutrition;
+
+    @ElementCollection
+    @CollectionTable(name = "product_ingredients", joinColumns = @JoinColumn(name = "product_id"))
+    @Column(name = "ingredient")
+    private Set<String> ingredients;
+
+    @ElementCollection
+    @CollectionTable(name = "product_warnings", joinColumns = @JoinColumn(name = "product_id"))
+    @Column(name = "warning")
+    private Set<String> warnings;
+
+    public Product() {}
 }
