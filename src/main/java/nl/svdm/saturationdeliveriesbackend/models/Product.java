cb01c0fc4c7b0fde69a01a8159a0afc933f0e552
stijnvdmeer <stijn08072002@outlook.com>
2025-04-13 10:57:37 +0200

    Added Getters and Setters to Product Model

--- src/main/java/nl/svdm/saturationdeliveriesbackend/models/Product.java
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/models/Product.java
@@ -46,4 +46,110 @@ public class Product {
     private Set<String> warnings;
 
     public Product() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public SubCategory getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(SubCategory subCategory) {
+        this.subCategory = subCategory;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Integer getPending() {
+        return pending;
+    }
+
+    public void setPending(Integer pending) {
+        this.pending = pending;
+    }
+
+    public Set<ProductLabel> getLabels() {
+        return labels;
+    }
+
+    public void setLabels(Set<ProductLabel> labels) {
+        this.labels = labels;
+    }
+    public void addLabel(ProductLabel label) {
+        this.labels.add(label);
+    }
+
+    public ProductNutrition getNutrition() {
+        return nutrition;
+    }
+
+    public void setNutrition(ProductNutrition nutrition) {
+        this.nutrition = nutrition;
+    }
+
+    public Set<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+    public void addIngredient(String ingredient) {
+        this.ingredients.add(ingredient);
+    }
+
+    public Set<String> getWarnings() {
+        return warnings;
+    }
+
+    public void setWarnings(Set<String> warnings) {
+        this.warnings = warnings;
+    }
+
+    public void addWarning(String warning) {
+        this.warnings.add(warning);
+    }
 }
