791dd103c99ae4141dba09cf52d7b1cad949a411
stijnvdmeer <stijn08072002@outlook.com>
2025-04-13 10:22:15 +0200

    Created OrderStatus ENUM for Order Model

---
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/models/enums/OrderStatus.java
@@ -0,0 +1,12 @@
+package nl.svdm.saturationdeliveriesbackend.models.enums;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED,
+    RETURNED,
+    FAILED,
+}
