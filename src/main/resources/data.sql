e1fa3a7b8bb066344619706d081d3aeee5cbe260
stijnvdmeer <stijn08072002@outlook.com>
2025-04-14 15:32:32 +0200

    testdata change

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,4 +11,5 @@ Values ('Pastas'),
 insert into products (calories, carbohydrates, fats, sugars, proteins, salts, name, price, stock, pending, category_id, sub_category_id)
 VALUES
     (10, 4, 3, 4, 5, 5, 'Taco Kruiden', 1.12, 10, 0, 2, 3),
-    (20, 4, 3, 4, 5, 1, 'Penne alle Ovu', 2, 5, 1, 1, 1);
+    (20, 4, 3, 4, 5, 1, 'Penne alle Ovu', 2, 5, 1, 1, 1),
+    (25, 1,2,3,4,5, 'Penne', 1, 5, 3, 1, 1)
