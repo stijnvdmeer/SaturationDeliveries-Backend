0f73be7627dc36e1892c0abf6d36e4e9d7f41326
stijnvdmeer <stijn08072002@outlook.com>
2025-04-14 17:00:06 +0200

    Added Getters and Setters

--- src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/categorydtos/CategoryOutputDto.java
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/categorydtos/CategoryOutputDto.java
@@ -3,4 +3,20 @@ package nl.svdm.saturationdeliveriesbackend.dtos.categorydtos;
 public class CategoryOutputDto {
     public Long id;
     public String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/productdtos/ProductAdminOutputDto.java
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/productdtos/ProductAdminOutputDto.java
@@ -19,4 +19,91 @@ public class ProductAdminOutputDto {
     public CategoryOutputDto category;
     public SubCategoryOutputDto subCategory;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Integer getPending() {
+        return pending;
+    }
+
+    public void setPending(Integer pending) {
+        this.pending = pending;
+    }
+
+    public List<String> getLabels() {
+        return labels;
+    }
+
+    public void setLabels(List<String> labels) {
+        this.labels = labels;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getWarnings() {
+        return warnings;
+    }
+
+    public void setWarnings(List<String> warnings) {
+        this.warnings = warnings;
+    }
+
+    public NutritionOutputDto getNutrition() {
+        return nutrition;
+    }
+
+    public void setNutrition(NutritionOutputDto nutrition) {
+        this.nutrition = nutrition;
+    }
+
+    public CategoryOutputDto getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryOutputDto category) {
+        this.category = category;
+    }
+
+    public SubCategoryOutputDto getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(SubCategoryOutputDto subCategory) {
+        this.subCategory = subCategory;
+    }
 }
