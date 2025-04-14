8dea6e51447b6f1afb2f736614391ad7bae2322d
stijnvdmeer <stijn08072002@outlook.com>
2025-04-14 10:42:50 +0200

    Created ProductAdminOutputDto

---
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/productdtos/ProductAdminOutputDto.java
@@ -0,0 +1,21 @@
+package nl.svdm.saturationdeliveriesbackend.dtos.productdtos;
+
+import nl.svdm.saturationdeliveriesbackend.dtos.categorydtos.CategoryOutputDto;
+import nl.svdm.saturationdeliveriesbackend.dtos.subcategorydtos.SubCategoryOutputDto;
+
+import java.util.List;
+
+public class ProductAdminOutputDto {
+    public Long id;
+    public String name;
+    public Double price;
+    public Integer stock;
+    public Integer pending;
+    public List<String> labels;
+    public List<String> ingredients;
+    public List<String> warnings;
+    public ProductNutritionDto nutrition;
+    public CategoryOutputDto category;
+    public SubCategoryOutputDto subCategory;
+
+}
