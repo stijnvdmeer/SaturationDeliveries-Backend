5d0650812deea08bf723dace2b56bec809b92909
stijnvdmeer <stijn08072002@outlook.com>
2025-04-14 15:21:15 +0200

    added getters and setters to ListOutputDto

--- src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/productdtos/ProductListOutputDto.java
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/productdtos/ProductListOutputDto.java
@@ -4,4 +4,28 @@ public class ProductListOutputDto {
     public String name;
     public Double price;
     public Integer stock;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
 }
