6a25c221004e2f42962e39d9667459b60dc19ba6
stijnvdmeer <stijn08072002@outlook.com>
2025-04-14 16:43:26 +0200

    refactured many methods and added more

--- src/main/java/nl/svdm/saturationdeliveriesbackend/repositories/ProductRepository.java
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/repositories/ProductRepository.java
@@ -10,6 +10,10 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategory_NameIgnoreCase(String category);
     List<Product> findBySubCategory_NameIgnoreCase(String subcategory);
-    List<Product> findByCategory_NameIgnoreCaseAndSubCategory_NameIgnoreCase(String category, String subcategory);
     List<Product> findByNameContainingIgnoreCase(String name);
+    List<Product> findByCategory_NameIgnoreCaseAndSubCategory_NameIgnoreCase(String category, String subCategory);
+    List<Product> findBySubCategory_NameIgnoreCaseAndNameContainingIgnoreCase(String subCategory, String name);
+    List<Product> findByCategory_NameIgnoreCaseAndNameContainingIgnoreCase(String category, String name);
+    List<Product> findByCategory_NameIgnoreCaseAndSubCategory_NameIgnoreCaseAndNameContainingIgnoreCase(String category, String subCategory, String name);
+
 }
