070956d377b16ad043f0a35047539c5e3144011f
stijnvdmeer <stijn08072002@outlook.com>
2025-04-14 13:42:58 +0200

    Fixed Methods in Product Repository

--- src/main/java/nl/svdm/saturationdeliveriesbackend/repositories/ProductRepository.java
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/repositories/ProductRepository.java
@@ -1,13 +1,15 @@
 package nl.svdm.saturationdeliveriesbackend.repositories;
 
+import nl.svdm.saturationdeliveriesbackend.models.Category;
 import nl.svdm.saturationdeliveriesbackend.models.Product;
+import nl.svdm.saturationdeliveriesbackend.models.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByCategory(String category);
-    List<Product> findBySubcategory(String subcategory);
-    List<Product> findByCategoryAndSubcategory(String category, String subcategory);
-    List<Product> findByNameContainingIgnoreCase(String name);
+    List<Product> findProductByCategory(Category category);
+    List<Product> findProductBySubCategory(SubCategory subcategory);
+    List<Product> findProductByCategoryAndSubCategory(Category category, SubCategory subcategory);
+    List<Product> findProductByNameContainingIgnoreCase(String name);
 }
