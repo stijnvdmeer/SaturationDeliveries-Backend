f3fc5f49c9da4fad189c65be655f91a83eec02cf
stijnvdmeer <stijn08072002@outlook.com>
2025-04-14 10:58:26 +0200

    Added Validation to NutritionInputDto

--- src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/embeddtos/NutritionInputDto.java
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/embeddtos/NutritionInputDto.java
@@ -1,10 +1,23 @@
 package nl.svdm.saturationdeliveriesbackend.dtos.embeddtos;
 
+import jakarta.validation.constraints.Digits;
+
 public class NutritionInputDto {
+    @Digits(integer = 4, fraction = 2, message = "Numerical value of calories is out of bounds")
     public Double calories;
+
+    @Digits(integer = 4, fraction = 2, message = "Numerical value of fats is out of bounds")
     public Double fats;
+
+    @Digits(integer = 4, fraction = 2, message = "Numerical value of carbohydrates is out of bounds")
     public Double carbohydrates;
+
+    @Digits(integer = 4, fraction = 2, message = "Numerical value of sugars is out of bounds")
     public Double sugars;
+
+    @Digits(integer = 4, fraction = 2, message = "Numerical value of proteins is out of bounds")
     public Double proteins;
+
+    @Digits(integer = 4, fraction = 2, message = "Numerical value of salts is out of bounds")
     public Double salts;
 }
