33ce999636bea0c27d0efe26bc553c94918f8c26
stijnvdmeer <stijn08072002@outlook.com>
2025-04-14 14:58:35 +0200

    Added Getters And Setters

--- src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/embeddtos/NutritionOutputDto.java
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/embeddtos/NutritionOutputDto.java
@@ -7,4 +7,52 @@ public class NutritionOutputDto {
     public Double sugars;
     public Double proteins;
     public Double salts;
+
+    public Double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(Double calories) {
+        this.calories = calories;
+    }
+
+    public Double getFats() {
+        return fats;
+    }
+
+    public void setFats(Double fats) {
+        this.fats = fats;
+    }
+
+    public Double getCarbohydrates() {
+        return carbohydrates;
+    }
+
+    public void setCarbohydrates(Double carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
+
+    public Double getSugars() {
+        return sugars;
+    }
+
+    public void setSugars(Double sugars) {
+        this.sugars = sugars;
+    }
+
+    public Double getProteins() {
+        return proteins;
+    }
+
+    public void setProteins(Double proteins) {
+        this.proteins = proteins;
+    }
+
+    public Double getSalts() {
+        return salts;
+    }
+
+    public void setSalts(Double salts) {
+        this.salts = salts;
+    }
 }
--- src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/productdtos/ProductDetailOutputDto.java
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/productdtos/ProductDetailOutputDto.java
@@ -12,4 +12,60 @@ public class ProductDetailOutputDto {
     public List<String> ingredients;
     public NutritionOutputDto nutrition;
     public List<String> warnings;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public List<String> getLabels() {
+        return labels;
+    }
+
+    public void setLabels(List<String> labels) {
+        this.labels = labels;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public NutritionOutputDto getNutrition() {
+        return nutrition;
+    }
+
+    public void setNutrition(NutritionOutputDto nutrition) {
+        this.nutrition = nutrition;
+    }
+
+    public List<String> getWarnings() {
+        return warnings;
+    }
+
+    public void setWarnings(List<String> warnings) {
+        this.warnings = warnings;
+    }
 }
