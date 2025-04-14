07e93f985966d38a70407cfb49f7c528fdc39ae3
stijnvdmeer <stijn08072002@outlook.com>
2025-04-14 10:55:13 +0200

    Created NutritionInputDto

---
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/embeddtos/NutritionInputDto.java
@@ -0,0 +1,10 @@
+package nl.svdm.saturationdeliveriesbackend.dtos.embeddtos;
+
+public class NutritionInputDto {
+    public Double calories;
+    public Double fats;
+    public Double carbohydrates;
+    public Double sugars;
+    public Double proteins;
+    public Double salts;
+}
