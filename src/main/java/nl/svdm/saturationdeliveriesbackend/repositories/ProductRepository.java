260667a06244fcca7d4513914d98e90b76197a7e
stijnvdmeer <stijn08072002@outlook.com>
2025-04-14 15:39:00 +0200

    refactured find by category and subcategory method

--- src/main/java/nl/svdm/saturationdeliveriesbackend/repositories/ProductRepository.java
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/repositories/ProductRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategory_NameIgnoreCase(String category);
     List<Product> findBySubCategory_NameIgnoreCase(String subcategory);
-    List<Product> findByCategory_NameAndSubCategory_NameIgnoreCase(String category, String subcategory);
+    List<Product> findByCategory_NameIgnoreCaseAndSubCategory_NameIgnoreCase(String category, String subcategory);
     List<Product> findByNameContainingIgnoreCase(String name);
 }
