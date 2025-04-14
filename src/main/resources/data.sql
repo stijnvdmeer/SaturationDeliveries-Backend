1b83c097c31f6a641363f4916490605d4d504ddc
stijnvdmeer <stijn08072002@outlook.com>
2025-04-14 13:43:17 +0200

    added data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into category(name)
+VALUES ('Italians'),
+('Mexicaans');
+
+insert into sub_category(name)
+Values ('Pastas'),
+       ('Sausen'),
+       ('Kruiden'),
+       ('Wraps');
+
+insert into products (calories, carbohydrates, fats, sugars, proteins, salts, name, price, stock, pending, category_id, sub_category_id)
+VALUES
+    (10, 4, 3, 4, 5, 5, 'Taco Kruiden', 1.12, 10, 0, 2, 3),
+    (20, 4, 3, 4, 5, 1, 'Penne alle Ovu', 2, 5, 1, 1, 1);
