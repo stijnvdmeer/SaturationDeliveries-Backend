15ae55a429d1c11cd7a45cd12aded3c49ad78749
stijnvdmeer <stijn08072002@outlook.com>
2025-04-14 10:07:07 +0200

    ProductNutritionDto

---
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/ProductNutritionDto.java
@@ -0,0 +1,10 @@
+package nl.svdm.saturationdeliveriesbackend.dtos;
+
+public class ProductNutritionDto {
+    public Double calories;
+    public Double fats;
+    public Double carbohydrates;
+    public Double sugars;
+    public Double proteins;
+    public Double salts;
+}
