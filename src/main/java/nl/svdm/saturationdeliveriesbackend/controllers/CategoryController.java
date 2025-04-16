dd46a31fb81810647e6b762a4698d1fb61699f6a
stijnvdmeer <stijn08072002@outlook.com>
2025-04-16 10:52:10 +0200

    Created update and delete functions Method

--- src/main/java/nl/svdm/saturationdeliveriesbackend/controllers/CategoryController.java
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/controllers/CategoryController.java
@@ -3,6 +3,7 @@ package nl.svdm.saturationdeliveriesbackend.controllers;
 import jakarta.validation.Valid;
 import nl.svdm.saturationdeliveriesbackend.dtos.categorydtos.CategoryInputDto;
 import nl.svdm.saturationdeliveriesbackend.dtos.categorydtos.CategoryOutputDto;
+import nl.svdm.saturationdeliveriesbackend.dtos.requestdtos.StringInputDto;
 import nl.svdm.saturationdeliveriesbackend.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -45,4 +46,21 @@ public class CategoryController {
     public ResponseEntity<CategoryOutputDto> getCategoryById(@PathVariable Long id) {
         return ResponseEntity.ok(categoryService.getCategoryById(id));
     }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<CategoryOutputDto>> searchCategory(@RequestBody StringInputDto inputDto) {
+        return ResponseEntity.ok(categoryService.searchCategoryByName(inputDto.name));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryOutputDto> updateCategory(@PathVariable Long id, @Valid @RequestBody CategoryInputDto dto) {
+        return ResponseEntity.ok(categoryService.updateCategory(dto, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/nl/svdm/saturationdeliveriesbackend/services/CategoryService.java
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/services/CategoryService.java
@@ -4,6 +4,7 @@ import nl.svdm.saturationdeliveriesbackend.dtos.categorydtos.CategoryInputDto;
 import nl.svdm.saturationdeliveriesbackend.dtos.categorydtos.CategoryOutputDto;
 import nl.svdm.saturationdeliveriesbackend.mappers.GenericMapper;
 import nl.svdm.saturationdeliveriesbackend.models.Category;
+import nl.svdm.saturationdeliveriesbackend.models.Product;
 import nl.svdm.saturationdeliveriesbackend.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -40,5 +41,26 @@ public class CategoryService {
                 .orElseThrow(); // TODO ResourceNotFoundException
 
         return genericMapper.singleToDto(category, CategoryOutputDto.class);
+     }
+
+     public List<CategoryOutputDto> searchCategoryByName(String name) {
+        List<Category> categories = categoryRepository.findByNameContainingIgnoreCase(name);
+        return genericMapper.listToDto(categories, CategoryOutputDto.class);
+     }
+
+     public CategoryOutputDto updateCategory(CategoryInputDto categoryInputDto, Long id) {
+         Category category = categoryRepository.findById(id)
+                 .orElseThrow(); //TODO ResourceNotFoundException
+
+         category.setName(categoryInputDto.getName());
+
+         categoryRepository.save(category);
+
+         return genericMapper.singleToDto(category, CategoryOutputDto.class);
+     }
+
+     public void deleteCategory(Long id) {
+        if(!categoryRepository.existsById(id)) throw new RuntimeException(""); //TODO ResourceNotFoundException
+         categoryRepository.deleteById(id);
      }
 }
