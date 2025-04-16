27ddfe0d0aa868507ad2d806af3fbc12166a3ebd
stijnvdmeer <stijn08072002@outlook.com>
2025-04-16 11:34:50 +0200

    Added Getters and Setters

--- src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/subcategorydtos/SubCategoryInputDto.java
+++ src/main/java/nl/svdm/saturationdeliveriesbackend/dtos/subcategorydtos/SubCategoryInputDto.java
@@ -5,4 +5,15 @@ import jakarta.validation.constraints.NotBlank;
 public class SubCategoryInputDto {
     @NotBlank(message = "name can not be empty")
     public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
+
+
